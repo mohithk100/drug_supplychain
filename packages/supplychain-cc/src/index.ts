c0a7b8fa62a45611263c89cfdc45e927afe83b4f
Mohith Khatri <mohithkhatri100@gmail.com>
2020-04-10 20:49:34 +0530

    updated index.ts supplychain-cc

--- packages/supplychain-cc/src/index.ts
+++ packages/supplychain-cc/src/index.ts
@@ -1,1 +1,6 @@
 export * from './models/Distributor.model';
+export * from './models/Manufacturer.model';
+export * from './models/Pharmacist.model';
+export * from './models/Supplier.model';
+export * from './models/drug.model'
+export * from './supplychain.controller';
